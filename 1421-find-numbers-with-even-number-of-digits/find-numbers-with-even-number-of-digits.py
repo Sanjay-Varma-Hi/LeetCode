1f5ec1ed6aad00b31bdfc86da6c62c2ac6652d0c
Sanjay Varma <153253384+Sanjay-Varma-Hi@users.noreply.github.com>
2025-05-12 20:29:52 -0700

    Time: 4 ms (33.46%) | Memory: 12.4 MB (58.75%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.py
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.py
@@ -2,6 +2,8 @@ class Solution(object):
     def findNumbers(self, nums):
         res = 0
         for n in nums:
-            if 9<n<100 or 999 < n < 10000 or n == 100000:
+            # if 9<n<100 or 999 < n < 10000 or n == 100000:
+            if len(str(n)) % 2 == 0:
                 res += 1
         return res
+    
