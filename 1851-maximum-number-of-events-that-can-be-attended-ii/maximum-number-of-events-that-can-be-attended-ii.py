e17c119e209125317b136407eebd362b59060d4e
Sanjay Varma <153253384+Sanjay-Varma-Hi@users.noreply.github.com>
2025-07-08 12:30:32 -0400

    Time: 1646 ms (66.67%) | Memory: 284.4 MB (11.11%) - LeetSync

--- 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.py
+++ 1851-maximum-number-of-events-that-can-be-attended-ii/maximum-number-of-events-that-can-be-attended-ii.py
@@ -2,40 +2,26 @@ from bisect import bisect_right
 
 class Solution:
     def maxValue(self, events, k):
-        # Sort events by end time
-        events.sort(key=lambda x: x[1])
+        events.sort()
+        starts = [e[0] for e in events]
         n = len(events)
 
-        # Extract start times for binary search
-        start_times = [event[0] for event in events]
+        memo = {}
 
-        # Initialize DP table: dp[i][j] = max value using first i events and at most j selections
-        dp = [[0] * (k + 1) for _ in range(n + 1)]
+        def dp(i, remaining):
+            if i == n or remaining == 0:
+                return 0
+            if (i, remaining) in memo:
+                return memo[(i, remaining)]
 
-        for i in range(1, n + 1):
-            start, end, value = events[i - 1]
-            # Find last non-overlapping event using binary search
-            prev = self.findLastNonOverlapping(events, i - 1, start)
+            # Option 1: Skip current event
+            skip = dp(i + 1, remaining)
 
-            for j in range(1, k + 1):
-                # Option 1: skip current event
-                # Option 2: take current event + best from prev
-                dp[i][j] = max(
-                    dp[i - 1][j],
-                    dp[prev + 1][j - 1] + value
-                )
+            # Option 2: Take current event
+            next_i = bisect_right(starts, events[i][1])
+            take = events[i][2] + dp(next_i, remaining - 1)
 
-        return dp[n][k]
+            memo[(i, remaining)] = max(skip, take)
+            return memo[(i, remaining)]
 
-    def findLastNonOverlapping(self, events, right, targetStart):
-        # Binary search to find last event ending before targetStart
-        left = 0
-        res = -1
-        while left <= right:
-            mid = (left + right) // 2
-            if events[mid][1] < targetStart:
-                res = mid
-                left = mid + 1
-            else:
-                right = mid - 1
-        return res
+        return dp(0, k)
