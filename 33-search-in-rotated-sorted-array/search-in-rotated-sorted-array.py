821bbde1f057332fe6034824a5cec0f4a699d1fe
Sanjay Varma <153253384+Sanjay-Varma-Hi@users.noreply.github.com>
2025-07-24 20:19:30 -0400

    Time: 0 ms (100.00%) | Memory: 12.8 MB (5.24%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,55 +1,25 @@
-# class Solution(object):
-#     def search(self, nums, target):
-#         l,r = 0, len(nums)-1
-#         while l < r:
-#             m = (l+r)//2
-#             if nums[m] > nums[r]:
-#                 l = m+1
-#             else:
-#                 r = m
-#         pi = l
-
-#         def bise(left, right):
-#             while left<right:
-#                 mid = (left+right) // 2
-#                 if nums[mid] == target:
-#                     return mid
-#                 elif nums[mid] < target:
-#                     left = mid +1
-#                 else:
-#                     right = mid
-#             return -1
-#         res = bise(0,pi-1)
-#         if res != -1:
-#             return res
-#         return bise(pi, len(nums) - 1)
-
-class Solution:
+class Solution(object):
     def search(self, nums, target):
-        l, r = 0, len(nums) - 1
-
+        l,r = 0, len(nums)-1
         while l < r:
-            m = (l + r) // 2
+            m = (l+r)//2
             if nums[m] > nums[r]:
-                l = m + 1
+                l = m+1
             else:
                 r = m
+        pi = l
 
-        pivot = l
-        
-        def binary_search(left,right):
-            while left <= right:
-                mid = (left + right) // 2
+        def bise(left, right):
+            while left<=right:
+                mid = (left+right) // 2
                 if nums[mid] == target:
                     return mid
                 elif nums[mid] < target:
-                    left = mid + 1
+                    left = mid +1
                 else:
-                    right = mid - 1
+                    right = mid -1
             return -1
-
-        result = binary_search(0, pivot - 1)
-        if result != -1:
-            return result
-        
-        return binary_search(pivot, len(nums) - 1)
+        res = bise(0,pi-1)
+        if res != -1:
+            return res
+        return bise(pi, len(nums) - 1)
