307b46a45b29a7ff90430b2b227e1abb0e33c323
Sanjay Varma <153253384+Sanjay-Varma-Hi@users.noreply.github.com>
2025-12-02 15:42:14 -0800

    Time: 15 ms (96.24%) | Memory: 16 MB (81.23%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,10 +1,22 @@
-from collections import defaultdict
-class Solution(object):
+# from collections import defaultdict
+# class Solution(object):
+#     def groupAnagrams(self, strs):
+#         anagram_map = defaultdict(list)  #creates a hashtable
+
+#         for word in strs:
+#             key = tuple(sorted(word)) #converts each word in str to sorted tuple
+#             anagram_map[key].append(word) #stores similar keys in a single list 
+
+
+#         return list(anagram_map.values())
+
+class Solution:
     def groupAnagrams(self, strs):
-        anagram_map = defaultdict(list)
+        ans = defaultdict(list)
 
-        for word in strs:
-            key = tuple(sorted(word))
-            anagram_map[key].append(word)
-
-        return list(anagram_map.values())
+        for s in strs:
+            key = "".join(sorted(s))
+            ans[key].append(s)
+        
+        return list(ans.values())
+        
